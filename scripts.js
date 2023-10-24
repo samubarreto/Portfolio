0921670dd0a11d64022b732850768317cff5fbad
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2023-10-24 13:21:12 -0300

    header progress

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function showMobileMenu() {
+  const menuMobile = document.querySelector('.mobile-menu');
+  
+  if (menuMobile.classList.contains('open')) {
+    menuMobile.classList.remove('open');
+    document.querySelector('.icon').src = 'source/menu-hamburger.png'
+  } else {
+    menuMobile.classList.add('open');
+    document.querySelector('.icon').src = 'source/x.png'
+  }
+}
